72be962f5b449cab3ce1a225770a4bd795fc5db7
brisque <65336391+thepoliticsguy@users.noreply.github.com>
2022-03-24 12:24:41 +1300

    player anim partial fix + jump force

--- dtm videogame 2/Assets/Scripts/PlayerController.cs
+++ dtm videogame 2/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     // Initalizing character speed and control
     private float speed = 10.0f;
     public float horizontalInput;
-    private float xRange = 12f;
+    private float xRange = 18f;
     private Animator playerAnim;
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
         playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -34,9 +36,15 @@ public class PlayerController : MonoBehaviour
         {
             playerAnim.SetTrigger("Run_trig");
         }
-        else if(horizontalInput < 0)
+        else if(horizontalInput == 0)
         {
+            playerAnim.ResetTrigger("Run_trig");
             playerAnim.SetTrigger("Idle_trig");
+        // Make Player jump when spacebar pressed
+        if(Input.GetKeyDown(KeyCode.Space))
+            {
+                playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            }
         }
     }
 }
