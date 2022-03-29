8049606893e44160d9216793674c939346dff5cf
brisque <65336391+thepoliticsguy@users.noreply.github.com>
2022-03-30 11:07:49 +1300

    added gravity modifier
    slow day

--- dtm videogame 2/Assets/Scripts/PlayerController.cs
+++ dtm videogame 2/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,17 @@ public class PlayerController : MonoBehaviour
     // Initalizing character speed and control
     private float speed = 10.0f;
     public float horizontalInput;
-    private float xRange = 18f;
+    private float xRange = 100f;
     private Animator playerAnim;
     private Rigidbody2D playerRb;
+    private float jumpForce = 10;
+    private float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
         playerAnim = GetComponent<Animator>();
         playerRb = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -32,19 +35,22 @@ public class PlayerController : MonoBehaviour
         // Moves player horizontally based on player input
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
         if (horizontalInput > 0 || horizontalInput < 0)
         {
             playerAnim.SetTrigger("Run_trig");
         }
         else if(horizontalInput == 0)
         {
-            playerAnim.ResetTrigger("Run_trig");
+            //playerAnim.ResetTrigger("Run_trig");
+            playerAnim.Rebind();
             playerAnim.SetTrigger("Idle_trig");
         // Make Player jump when spacebar pressed
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) || (Input.GetKeyDown(KeyCode.Space) && Input.GetKeyDown(KeyCode.RightArrow)))
             {
+                Debug.Log("In jump");
                 playerAnim.SetTrigger("Jump_trig");
-                playerRb.AddForce(transform.up * 10, ForceMode2D.Impulse);
+                playerRb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
             }
         }
     }
