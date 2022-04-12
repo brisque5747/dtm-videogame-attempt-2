afef66eed8e2d3d2c963fea5135019d83ef89254
brisque <65336391+thepoliticsguy@users.noreply.github.com>
2022-04-13 11:10:46 +1200

    spawn manager creation

--- dtm videogame 2/Assets/Scripts/SpawnManager.cs
+++ dtm videogame 2/Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 1;
+    private float spawnInterval = 2.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -16,4 +20,9 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], new Vector3(25, 3, 0), animalPrefabs[animalIndex].transform.rotation);
+    } 
 }
